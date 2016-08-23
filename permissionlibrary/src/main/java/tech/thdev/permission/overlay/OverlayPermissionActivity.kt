563deed051d3be354353e314801145dfeceab78a
Taehwan <taehwan@thdev.net>
2016-08-23 18:03:31 +0900

    OverlayPermission Activity sample update.

---
+++ permissionlibrary/src/main/java/tech/thdev/permission/overlay/OverlayPermissionActivity.kt
@@ -0,0 +1,14 @@
+package tech.thdev.permission.overlay
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Tae-hwan on 8/23/16.
+ */
+class OverlayPermissionActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
